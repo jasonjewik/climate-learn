920cd95da65056a34ec3863f86046738356842d0
jasonjewik <jason.jewik@cs.ucla.edu>
2023-03-28 15:07:53 -0700

    Added pretraining

--- src/climate_learn/models/modules/__init__.py
+++ src/climate_learn/models/modules/__init__.py
@@ -1,3 +1,4 @@
 from .forecast import ForecastLitModule
 from .downscale import DownscaleLitModule
 from .linear import LinearLitModule
+from .pretraining import PretrainingLitModule
---
+++ src/climate_learn/models/modules/pretraining.py
@@ -0,0 +1,129 @@
+from .utils.lr_scheduler import LinearWarmupCosineAnnealingLR
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+
+class PretrainingLitModule(pl.LightningModule):
+    def __init__(
+        self,
+        net1,
+        net2,
+        lr=5e-4,
+        weight_decay=0.2,
+        warmup_epochs=5,
+        max_epochs=100,
+        warmup_start_lr=1e-8,
+        eta_min=1e-8,
+        init_temp=np.log(0.07),
+        max_temp=np.log(100),
+        temp_lr=1e-2
+    ):
+        super().__init__()
+        self.save_hyperparameters(logger=False, ignore=["net"])
+        self.net1 = net1
+        self.net2 = net2
+        self.optim_cls = torch.optim.AdamW
+        self.temperature = torch.tensor([init_temp], requires_grad=True)
+        self.max_temp = max_temp
+        self.temp_lr = temp_lr
+
+    def forward(self, x):
+        enc1 = F.normalize(self.net1(x[:,:,0,:,:]))
+        enc2 = F.normalize(self.net2(x[:,:,1,:,:]))
+        return torch.stack(enc1, enc2)
+    
+    def configure_optimizers(self):
+        params = [
+            {
+                "params": self.net.parameters(),
+                "lr": self.hparams.lr,
+                "weight_decay": self.hparams.weight_decay
+            },
+            {
+                "params": self.temperature,
+                "lr": self.temp_lr
+            }
+        ]
+        optimizer = self.optim_cls(params)
+
+        lr_scheduler = LinearWarmupCosineAnnealingLR(
+            optimizer,
+            self.hparams.warmup_epochs,
+            self.hparams.max_epochs,
+            self.hparams.warmup_start_lr,
+            self.hparams.eta_min
+        )
+
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+    
+    def training_step(self, batch, batch_idx):
+        x = batch[0]
+        encodings = self(x)
+        logit_scale = torch.exp(self.temperature)
+        logits = logit_scale * torch.stack(
+            torch.matmul(encodings[0], encodings[1].T),
+            torch.matmul(encodings[1], encodings[0].T)
+        )
+        labels = torch.arange(x.shape[0])
+        loss = (
+            F.cross_entropy(logits[0], labels)
+            + F.cross_entropy(logits[1], labels)
+        ) / 2
+        self.log(
+            "train_loss",
+            loss,
+            on_step=True,
+            on_epoch=False,
+            prog_bar=True,
+            batch_size=len(x)
+        )
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x = batch[0]
+        encodings = self(x)
+        logit_scale = torch.exp(self.temperature)
+        logits = logit_scale * torch.stack(
+            torch.matmul(encodings[0], encodings[1].T),
+            torch.matmul(encodings[1], encodings[0].T)
+        )
+        labels = torch.arange(x.shape[0])
+        loss = (
+            F.cross_entropy(logits[0], labels)
+            + F.cross_entropy(logits[1], labels)
+        ) / 2
+        self.log(
+            "val_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=False,
+            sync_dist=True,
+            batch_size=len(x)
+        )
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x = batch[0]
+        encodings = self(x)
+        logit_scale = torch.exp(self.temperature)
+        logits = logit_scale * torch.stack(
+            torch.matmul(encodings[0], encodings[1].T),
+            torch.matmul(encodings[1], encodings[0].T)
+        )
+        labels = torch.arange(x.shape[0])
+        loss = (
+            F.cross_entropy(logits[0], labels)
+            + F.cross_entropy(logits[1], labels)
+        ) / 2
+        self.log(
+            "test_loss",
+            loss,
+            on_step=False,
+            on_epoch=True,
+            sync_dist=True,
+            batch_size=len(x)
+        )
+        return loss
