6ee43cec87b99e4d2837526973f271302acc15f5
jasonjewik <jason.jewik@cs.ucla.edu>
2023-03-28 14:38:17 -0700

    Added ResNet encoder

--- src/climate_learn/models/components/resnet.py
+++ src/climate_learn/models/components/resnet.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from .cnn_blocks import PeriodicConv2D, ResidualBlock
+from .cnn_blocks import PeriodicConv2D, ResidualBlock, Downsample
 
 # Large based on https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/diffusion/ddpm/unet.py
 # MIT License
@@ -99,3 +99,64 @@ class ResNet(nn.Module):
             m(pred, y, transform, out_variables, lat, clim, log_postfix)
             for m in metrics
         ], pred
+
+
+class ResNetEncoder(nn.Module):
+    def __init__(
+        self,
+        hidden_channels=128,
+        embedding_dim=512,
+        dropout=0.1,
+        n_blocks=16,
+        downsample_every=5,
+        img_size=(32,64)
+    ):
+        super().__init__()
+        self.in_channels = 1
+        self.out_channels = 1
+        self.hidden_channels = hidden_channels
+        self.embedding_dim = embedding_dim
+        self.activation = nn.LeakyReLU(0.3)
+
+        self.image_proj = PeriodicConv2D(
+            self.in_channels,
+            self.hidden_channels,
+            kernel_size=7,
+            padding=3
+        )
+
+        blocks = []
+        num_downsample = 0
+        for i in range(n_blocks):
+            blocks.append(
+                ResidualBlock(
+                    self.hidden_channels,
+                    self.hidden_channels,
+                    activation="leaky",
+                    norm=True,
+                    dropout=dropout
+                )
+            )
+            if divmod(i, downsample_every)[1] == downsample_every - 1:
+                blocks.append(Downsample(self.hidden_channels))
+                num_downsample += 1
+
+        self.blocks = nn.ModuleList(blocks)
+        self.norm = nn.BatchNorm2d(self.hidden_channels)
+        linear_in_dim = (
+            self.hidden_channels
+            * img_size[0]//2**num_downsample
+            * img_size[1]//2**num_downsample
+        )
+        self.final = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(linear_in_dim, self.embedding_dim)
+        )
+
+    def forward(self, x):
+        x = self.image_proj(x)
+        for block in self.blocks:
+            x = block(x)
+        pred = self.final(self.activation(self.norm(x)))
+        return pred
+        
